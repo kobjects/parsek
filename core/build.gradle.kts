34293010406923fc5dcb0d14874f15d15ab90aef
Stefan Haustein <stefan.haustein@gmail.com>
2023-01-10 21:01:59 +0100

    Assign version number to recent changes before tokenizer/scanner split

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 // CocoaPods requires the podspec to have a version.
 group = "org.kobjects.parserlib"
-version = "0.4.1"
+version = "0.4.2"
 
 kotlin {
     android {
--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library")
 }
 
-version = "0.4.1"
+version = "0.4.2"
 
 kotlin {
     android()
