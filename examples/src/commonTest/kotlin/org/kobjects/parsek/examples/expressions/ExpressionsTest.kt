0c11ddda743708d7e17b6cc58f9c6ab68bc8a08a
Stefan Haustein <stefan.haustein@gmail.com>
2024-06-23 21:50:21 +0200

    Drop the calculator example in favour of the corresponding (revived) test

--- examples/src/commonTest/kotlin/org/kobjects/parsek/examples/expressions/ExpressionsTest.kt
+++ examples/src/commonTest/kotlin/org/kobjects/parsek/examples/expressions/ExpressionsTest.kt
@@ -10,6 +10,6 @@ class ExpressionsTest {
         val ctx = RootContext
         assertEquals(-4.0, ctx.eval("4-4-4"))
         // assertEquals("Hello", ctx.eval("left(\"HelloWorld\", 5)"))
-        // assertEquals(4.0, ctx.eval("2^2"))
+        assertEquals(4.0, ctx.eval("2^2"))
     }
 }
